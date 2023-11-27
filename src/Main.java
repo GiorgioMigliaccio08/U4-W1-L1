1b79c685540c91a843f2ee57b8d84f2d124c47c5
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2023-11-27 16:52:59 +0100

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,1 @@
-public class Main {
-    public static void main(String[]arg) {
-        int PrimoNumero = 4;
-        int SecondoNumero = 8;
-        int Prodotto = PrimoNumero * SecondoNumero;
 
-        System.out.println("Il risultato della moltiplicazione è:" + Prodotto);
-    }
-}
